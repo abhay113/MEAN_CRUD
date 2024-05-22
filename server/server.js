62ab6a45665236e8ca445fc1339dbbdfcd10ab65
abhay <abhay@cateina.com>
2024-05-22 10:08:57 +0530

    basic setup

---
+++ server/server.js
@@ -0,0 +1,35 @@
+//imports
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+
+//middlewares
+const app = express();
+mongoose.Promise = global.Promise;
+app.use(cors())
+app.use(bodyParser.json())
+
+
+//global variables
+const URL = "mongodb://localhost:27017/employeeDetails"
+const PORT = 3000;
+// const employeeRoutes = require('./empo')
+
+
+
+mongoose.connect(URL).then(()=>{console.log("database is connected")},err=>{
+    console.log(`something went wrong ${err}`)
+})
+
+// app.use('/employees', employeeRoutes);
+
+app.get("/", (req, res) => {
+    res.send("hello ")
+})
+
+app.listen(PORT, (err) => {
+    console.log("server is running on port " + PORT)
+})
